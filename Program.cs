79765cc03c004147984b5aff99de1a8bdd7d7260
Renato Ryan <renatoryan312@gmail.com>
2026-05-09 14:23:15 -0300

    Adiciona sistema de batalha em turnos

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Sistema_de_Batalha_em_Turnos;
+
+Unidade jogador = new Unidade(100, 20, 12, "Jogador");
+Unidade inimigo = new Unidade(75, 10, 7, "Inimigo");
+Random random = new Random();
+
+while (!jogador.estaMorto && !inimigo.estaMorto)
+{
+    Console.WriteLine(jogador.NomeUnidade + " HP = " + jogador.Hp + " " + inimigo.NomeUnidade + " HP = " + inimigo.Hp);
+    Console.WriteLine("Turno do Jogador! O que você irá fazer? [A] Atacar [C] Curar");
+    string choice = Console.ReadLine();
+    choice = choice.ToLower();
+
+    if (choice == "a")
+    {
+        jogador.Ataque(inimigo);
+    }
+    else if (choice == "c")
+    {
+        jogador.Curar();
+    }
+    else
+    {
+        Console.WriteLine("Comando inválido!");
+    }
+        
+    Console.WriteLine("Pressione ENTER para continuar");
+    Console.ReadLine();
+    Console.Clear();
+
+    if (jogador.estaMorto || inimigo.estaMorto)
+        break;
+
+    Console.WriteLine(jogador.NomeUnidade + " HP = " + jogador.Hp + ". " + inimigo.NomeUnidade + " HP = " + inimigo.Hp);
+    Console.WriteLine("Turno do Inimigo!");
+
+    int rand = random.Next(0, 2);
+
+    if (rand == 0)
+    {
+        inimigo.Ataque(jogador);
+    }
+    else
+        inimigo.Curar();
+}
---
+++ Unidade.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Batalha_em_Turnos
+{
+    internal class Unidade
+    {
+        private int hpAtual;
+        private int hpMaximo;
+        private int poderAtaque;
+        private int poderCura;
+        private string nomeUnidade;
+        private Random random;
+
+        public int Hp { get { return hpAtual; } }
+
+        public string NomeUnidade { get { return nomeUnidade; } }
+
+        public bool estaMorto { get { return hpAtual <= 0; } }
+        public Unidade(int hpMaximo, int poderAtaque, int poderCura, string nomeUnidade)
+        {
+            this.hpMaximo = hpMaximo;
+            this.hpAtual = hpMaximo;
+            this.poderAtaque = poderAtaque;
+            this.poderCura = poderCura;
+            this.nomeUnidade = nomeUnidade;
+            this.random = new Random();
+        }
+
+        public void Ataque(Unidade unidadeAtacar)
+        {
+            double rng = random.NextDouble();
+            rng = rng / 2 + 0.75f;
+            int ranDano = (int)(poderAtaque * rng);
+            unidadeAtacar.levarDano(ranDano);
+            Console.WriteLine(nomeUnidade + " Ataca " + unidadeAtacar.nomeUnidade + " e dá " + ranDano + " de Dano de Ataque! ");
+            Console.WriteLine("-------------------------");
+        }
+
+        public void levarDano(int dano)
+        {
+            hpAtual -= dano;
+
+            if (estaMorto)
+            {
+                Console.WriteLine(NomeUnidade + " foi derrotado(a)!");
+            }
+        }
+
+        public void Curar()
+        {
+            if (hpAtual == hpMaximo)
+                Console.WriteLine($"O HP do(a) {nomeUnidade} está no máximo!");
+            else if (hpAtual < (hpMaximo * 40) / 100)
+            {
+                double rng = random.NextDouble();
+                rng = rng / 2 + 0.75f;
+                int cura = (int)(poderCura * rng);
+                hpAtual = cura + hpAtual > hpMaximo ? hpMaximo : hpAtual + cura;
+                Console.WriteLine(NomeUnidade + " se cura e recupera " + cura + " de HP! ");
+            }
+            
+        }
+    }
+}
